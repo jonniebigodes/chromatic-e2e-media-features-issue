ccfe63884b5a73f6cd1889a19230c0782930ec90
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2024-09-07 18:55:58 +0100

    Re-introduces selector with wait

--- cypress/e2e/Auth.cy.js
+++ cypress/e2e/Auth.cy.js
@@ -11,6 +11,10 @@ describe('Authentication - with high contrast', () => {
 
       cy.visit('/login')
 
+    // cy.wait(2000) // Works locally and generates a snapshot file correctly
+
+     cy.get('input[name="email"]').focus({timeout: 2000})
+
         // Take a snapshot of the initial state of the login page
       cy.takeSnapshot('Cypress - High Contrast - Initial state of the login page')
       cy.screenshot('Cypress - High Contrast - Initial state of the login page')
@@ -36,7 +40,11 @@ describe('Authentication - with reduced motion', () => {
 
 
     cy.visit('/login')
-    
+
+    // cy.wait(2000) // Works locally and generates a snapshot file correctly
+
+    cy.get('input[name="email"]').focus({timeout: 2000})
+
     // Take a snapshot of the initial state of the login page
     cy.takeSnapshot('Cypress - Reduced Motion - Initial state of the login page')
     cy.screenshot('Cypress - Reduced Motion - Initial state of the login page')
